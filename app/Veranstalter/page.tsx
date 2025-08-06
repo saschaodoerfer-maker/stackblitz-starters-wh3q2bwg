9b75ad3d4cc92a04fd4017356fb76564f98eb22b
saschaodoerfer-maker <saschaodoerfer@gmail.com>
2025-08-06 14:47:04 +0200

    Delete app/Veranstalter directory

--- app/Veranstalter/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Veranstalter() {
-  return (
-    <div style={{ padding: '2rem' }}>
-      <h1>📅 Veranstalterbereich</h1>
-      <p>Hier kannst du Basartermine verwalten.</p>
-    </div>
-  );
-}
