587187534f31b8e2dd612f99411ef819edfabc96
saschaodoerfer-maker <saschaodoerfer@gmail.com>
2025-08-06 14:47:13 +0200

    Delete app/Verkäufer directory

--- app/Verkäufer/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Verkaeufer() {
-  return (
-    <div style={{ padding: '2rem' }}>
-      <h1>🧾 Verkäuferbereich</h1>
-      <p>Hier kannst du Artikel einreichen und deine Nummer verwalten.</p>
-    </div>
-  );
-}
