13322619751af69bf594a82d4934023d9879243e
saschaodoerfer-maker <saschaodoerfer@gmail.com>
2025-08-06 13:11:41 +0200

    Create page.tsx

---
+++ app/pages/Verkäufer/page.tsx
@@ -0,0 +1,8 @@
+export default function Verkaeufer() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>🧾 Verkäuferbereich</h1>
+      <p>Hier kannst du Artikel einreichen und deine Nummer verwalten.</p>
+    </div>
+  );
+}
