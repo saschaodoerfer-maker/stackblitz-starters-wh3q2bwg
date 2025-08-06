ea25b22262d00427ce8a7c4d3630ccc375220a8c
saschaodoerfer-maker <saschaodoerfer@gmail.com>
2025-08-06 14:27:47 +0200

    Create page.tsx

---
+++ app/Veranstalter/page.tsx
@@ -0,0 +1,8 @@
+export default function Veranstalter() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>📅 Veranstalterbereich</h1>
+      <p>Hier kannst du Basartermine verwalten.</p>
+    </div>
+  );
+}
