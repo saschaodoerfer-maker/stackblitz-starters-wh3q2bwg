03bd305b155732a223b93ccb6c316881a5335570
saschaodoerfer-maker <saschaodoerfer@gmail.com>
2025-08-06 13:10:01 +0200

    Delete app/pages/veranstalter.tsx

--- app/pages/veranstalter.tsx
+++
@@ -1,8 +0,0 @@
-export default function VeranstalterPage() {
-  return (
-    <main style={{ padding: '2rem', fontFamily: 'Arial' }}>
-      <h1>📅 Veranstalterbereich</h1>
-      <p>Hier kannst du Termine anlegen und Verkäufer freischalten.</p>
-    </main>
-  );
-}
